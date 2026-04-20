1ff738613e144dbb4bee56b3c4ce488c036a5f27
oleg.bondarenko <mnkrp.ml@gmail.com>
2026-04-20 18:58:22 +0200

    copied old app, lib and e2e files into an empty expo project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+
+import { registerRootComponent } from 'expo';
+import "reflect-metadata";
+import './locales/index';
+
+import App from './ui/app';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
