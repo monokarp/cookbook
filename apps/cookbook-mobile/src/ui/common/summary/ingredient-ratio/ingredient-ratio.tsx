ff0e934bdb108af4b1c9d99f6afb5d6d84d6b205
oleg.bondarenko <mnkrp.ml@gmail.com>
2026-04-20 17:37:24 +0200

    ingredient ratio increments WIP

--- apps/cookbook-mobile/src/ui/common/summary/ingredient-ratio/ingredient-ratio.tsx
+++ apps/cookbook-mobile/src/ui/common/summary/ingredient-ratio/ingredient-ratio.tsx
@@ -1,33 +1,48 @@
-import { View } from "react-native";
-import { Text, ToggleButton } from "react-native-paper";
-import { styles } from "./ingredient-ratio.style";
+import { View } from 'react-native';
+import { Text, ToggleButton } from 'react-native-paper';
+import { styles } from './ingredient-ratio.style';
 
 interface RatioProps {
-    value: number;
-    incrementValue?: number;
-    onChange: (value: number) => void;
+  value: number;
+  smallIncrement?: number;
+  largeIncrement?: number;
+  onChange: (value: number) => void;
 }
 
-const round = num => Number((num).toFixed(1));
+const round = (num) => Number(num.toFixed(1));
 
-export function IngredientRatio({ value: ratio, incrementValue = 0.5, onChange }: RatioProps) {
-    const increment = () => onChange(round(ratio + incrementValue));
-    const decrement = () => onChange(round(ratio - incrementValue ? ratio - incrementValue : ratio));;
+export function IngredientRatio({
+  value: ratio,
+  smallIncrement = 0.1,
+  largeIncrement = 1,
+  onChange,
+}: RatioProps) {
+  const increment = (value: number) => onChange(round(ratio + value));
+  const decrement = (value: number) =>
+    onChange(round(ratio - value ? ratio - value : ratio));
 
-    return (
-        <View style={styles.boxContainer}>
-            <Text style={styles.label} variant="headlineSmall">{ratio}</Text>
-            {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
-            <ToggleButton.Row style={{ margin: 10 }} onValueChange={() => { }} value="">
-                <ToggleButton
-                    icon="minus"
-                    onPress={decrement}
-                />
-                <ToggleButton
-                    icon="plus"
-                    onPress={increment}
-                />
-            </ToggleButton.Row>
-        </View>
-    );
+  return (
+    <View style={styles.boxContainer}>
+      <Text style={styles.label} variant="headlineSmall">
+        {ratio}
+      </Text>
+      <ToggleButton.Row
+        style={{ margin: 10 }}
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        onValueChange={() => {}}
+        value=""
+      >
+        <ToggleButton icon="minus" onPress={() => decrement(largeIncrement)} />
+        <ToggleButton
+          icon="minus-thick"
+          onPress={() => decrement(smallIncrement)}
+        />
+        <ToggleButton icon="plus" onPress={() => increment(smallIncrement)} />
+        <ToggleButton
+          icon="plus-thick"
+          onPress={() => increment(largeIncrement)}
+        />
+      </ToggleButton.Row>
+    </View>
+  );
 }
